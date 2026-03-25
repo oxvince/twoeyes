151fa496597acac6db60c5bdafa33acfb5bf091d
Vince <48380956+oxvince@users.noreply.github.com>
2026-03-25 11:27:23 +0800

    Refactor Seo component to use static metadata

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -1,80 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
-import { useStaticQuery, graphql } from "gatsby";
 
-/**
- * Seo component props type
- */
 type SeoProps = {
   description?: string;
   lang?: string;
   meta?: ({ property?: string; content: string; name?: string } | undefined | null)[];
 };
 
-/**
- * Seo component to be included into various pages
- * @param description - of a page
- * @param lang - of a page
- * @param meta - of a page
- */
 const Seo: React.FC<SeoProps> = ({ description = "", lang = "en", meta = [] }) => {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `,
-  );
-
-  const metaDescription = description || site.siteMetadata.description;
-
+  const siteMetadata = {
+    title: "twoeyes",
+    description: "Restoring binocular vision",
+    author: "artelydev",
+  };
+  const metaDescription = description || siteMetadata.description;
   const defaultMetaData: object[] = [
-    {
-      name: `description`,
-      content: metaDescription,
-    },
-    {
-      property: `og:title`,
-      content: "twoeyes | become binocular",
-    },
-    {
-      property: `og:description`,
-      content: metaDescription,
-    },
-    {
-      property: `og:type`,
-      content: `website`,
-    },
-    {
-      name: `twitter:card`,
-      content: `summary`,
-    },
-    {
-      name: `twitter:creator`,
-      content: site.siteMetadata.author,
-    },
-    {
-      name: `twitter:title`,
-      content: "twoeyes | become binocular",
-    },
-    {
-      name: `twitter:description`,
-      content: metaDescription,
-    },
+    { name: `description`, content: metaDescription },
+    { property: `og:title`, content: "twoeyes | become binocular" },
+    { property: `og:description`, content: metaDescription },
+    { property: `og:type`, content: `website` },
+    { name: `twitter:card`, content: `summary` },
+    { name: `twitter:creator`, content: siteMetadata.author },
+    { name: `twitter:title`, content: "twoeyes | become binocular" },
+    { name: `twitter:description`, content: metaDescription },
   ];
-
   return (
     <Helmet
-      htmlAttributes={{
-        lang,
-      }}
+      htmlAttributes={{ lang }}
       title="twoeyes | become binocular"
       meta={defaultMetaData.concat(meta)}
     />
